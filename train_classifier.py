52f098a98e1ebecfece4efa5cf81a57110fe36ec
mahumsajid <mahumxsajid@gmail.com>
2024-11-25 23:17:58 -0500

    Added necessary libraries and added function create_model as our neural network

--- train_classifier.py
+++ train_classifier.py
@@ -1,7 +1,8 @@
 import pickle
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+import tensorflow as tf
+from tensorflow.keras import layers, models, Input
 import numpy as np
 
 # load data
@@ -19,7 +20,24 @@ data = data.reshape(data.shape[0], -1)
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 # train model 
-model = RandomForestClassifier(n_estimators=200, max_depth=15, random_state=42)
+def create_model():
+    model = models.Sequential([
+        Input(shape=(128, 128, 3)),
+        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(len(labels), activation='sigmoid') # Sigmoid for number of classes
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+# create instance of new model
+model = create_model()
 
 # train classifers
 model.fit(x_train, y_train)
