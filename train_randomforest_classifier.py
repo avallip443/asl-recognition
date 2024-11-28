4060de72d17b79d8ff03255946bd251e925c1686
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-11-27 22:59:13 -0500

    rename files

--- train_randomforest_classifier.py
+++ train_randomforest_classifier.py
@@ -1,12 +1,11 @@
 import pickle
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
 
-data_dict = pickle.load(open('./Adata.pickle', 'rb'))
+data_dict = pickle.load(open('./data.pickle', 'rb'))
 
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
@@ -23,6 +22,6 @@ score = accuracy_score(y_predict, y_test)
 
 print('{}% of samples were classified correctly !'.format(score * 100))
 
-f = open('Aforest_model.p', 'wb')
+f = open('randomforest_model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
