55134e4d53d10c1b25deb3198437e5b1b2596107
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-11-27 23:29:13 -0500

    add comments

--- train_svm_classifier.py
+++ train_svm_classifier.py
@@ -4,31 +4,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-# Load data
-data_dict = pickle.load(open('./Adata.pickle', 'rb'))
-
+# load data
+data_dict = pickle.load(open('./data.pickle', 'rb'))
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
 
-# Split the data into training and testing sets
+# split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
-# Initialize the SVM model with RBF kernel
+# initialize the SVM model with RBF kernel
 model = SVC(kernel='rbf')
 
-# Train the model
+# train the model
 model.fit(x_train, y_train)
 
-# Make predictions
+# make predictions
 y_predict = model.predict(x_test)
 
-# Calculate the accuracy score
+# calculate the accuracy score
 score = accuracy_score(y_predict, y_test)
 
-# Output the accuracy
+# output the accuracy
 print('{}% of samples were classified correctly!'.format(score * 100))
 
-# Save the model
-f = open('Asvm_model.p', 'wb')
+# save the model
+f = open('svm_model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
