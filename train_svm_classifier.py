d9b2b741e1996526b1886236203cbf4db6155e8a
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-11-30 16:10:45 -0500

    update normalization

--- train_svm_classifier.py
+++ train_svm_classifier.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import normalize
 import numpy as np
 
 # load data
@@ -11,6 +12,9 @@ labels = np.asarray(data_dict['labels'])
 
 # split the data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+
+normalize(x_train)
+normalize(x_test)
 
 # initialize the SVM model with RBF kernel
 model = SVC(kernel='rbf')
@@ -28,6 +32,6 @@ score = accuracy_score(y_predict, y_test)
 print('{}% of samples were classified correctly!'.format(score * 100))
 
 # save the model
-f = open('svm_model.p', 'wb')
+f = open('Asvm_model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
